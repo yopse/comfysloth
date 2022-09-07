f6239f66e1afea0963d0fed77da83dd6a0551bbd
yopse <82827725+yopse@users.noreply.github.com>
2022-09-07 15:48:29 +0530

    ReactRouter Navbar CartButtons Footer is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
-
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 function App() {
-  return <h4>comfy sloth </h4>;
+  return (
+    <Router>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+
+        <Route path="/about">
+          <About> </About>
+        </Route>
+        <Route path="/cart">
+          <Cart></Cart>
+        </Route>
+        <Route path="/products">
+          <Products></Products>
+        </Route>
+        <Route path="/products/:id" children={<SingleProduct />}></Route>
+
+        <Route path="/checkout">
+          <Checkout></Checkout>
+        </Route>
+
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
 export default App;
