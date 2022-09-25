8ce6908106890cbaefaa9d915614e7383a1b80e8
yopse <82827725+yopse@users.noreply.github.com>
2022-09-25 17:35:08 +0530

    AuthWrapper is added and fixed

--- src/App.js
+++ src/App.js
@@ -10,41 +10,44 @@ import {
   Error,
   Checkout,
   PrivateRoute,
+  AuthWrapper,
 } from "./pages";
 function App() {
   return (
-    <Router>
-      <Navbar></Navbar>
-      <Sidebar></Sidebar>
+    <AuthWrapper>
+      <Router>
+        <Navbar></Navbar>
+        <Sidebar></Sidebar>
 
-      <Switch>
-        <Route exact path="/">
-          <Home></Home>
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
 
-        <Route exact path="/about">
-          <About> </About>
-        </Route>
-        <Route exact path="/cart">
-          <Cart></Cart>
-        </Route>
-        <Route exact path="/products">
-          <Products></Products>
-        </Route>
-        <Route exact path="/products/:id">
-          <SingleProduct />
-        </Route>
+          <Route exact path="/about">
+            <About> </About>
+          </Route>
+          <Route exact path="/cart">
+            <Cart></Cart>
+          </Route>
+          <Route exact path="/products">
+            <Products></Products>
+          </Route>
+          <Route exact path="/products/:id">
+            <SingleProduct />
+          </Route>
 
-        <PrivateRoute exact path="/checkout">
-          <Checkout></Checkout>
-        </PrivateRoute>
+          <PrivateRoute exact path="/checkout">
+            <Checkout></Checkout>
+          </PrivateRoute>
 
-        <Route exact path="*">
-          <Error></Error>
-        </Route>
-      </Switch>
-      <Footer></Footer>
-    </Router>
+          <Route exact path="*">
+            <Error></Error>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </AuthWrapper>
   );
 }
 
