1daa99566a361c5afb4bc54802d14cae85e5fd10
yopse <82827725+yopse@users.noreply.github.com>
2022-09-25 15:24:13 +0530

    Auth0 is setup & userContext is setup

--- src/index.js
+++ src/index.js
@@ -7,14 +7,27 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+// dev - qj07rra5.us.auth0.com;
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev - qj07rra5.us.auth0.com"
+    clientId="CpwogLdwZ1k1x4F9E1ETB7L2NJaWhI5d"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
+
+// CpwogLdwZ1k1x4F9E1ETB7L2NJaWhI5d;
+
+// P7UeVb5dePXju0pX60uXllwuU6q3mNeNsHFxrM4pee4iswBQxax5SEMOcRbyyPJC;
